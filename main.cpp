f4bf36f26ce77fd79a83f872ca0082fd866ac72d
Rodríguez Poggio, Darío Andrés <rodriguez.dario.a@gmail.com>
2021-08-02 00:55:05 -0300

    Soporte para excepciones en main()
    Signed-off-by: Rodríguez Poggio, Darío Andrés <rodriguez.dario.a@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
+#include <iostream>
 #include "restful.hpp"
 
 
 
 int main (const int, const char**)
 {
-    auto restfulControl = std::make_shared<Control>();
-    return restfulControl->run();
+    try {
+        auto restfulControl = std::make_shared<Control>();
+        return restfulControl->run();
+    }
+    catch (...) {
+        std::cerr << "Error en el Controlador principal. Abortado." << std::endl;
+    }
 }
 
